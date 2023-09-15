231877ef37ef44e6e880cd43760bc5184eff5c60
vipin <vipinkumarlamba55@gmail.com>
2023-09-15 17:11:18 +0530

    mobile menu with hamburger and background part completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,22 @@ import logo from '../assets/img/logo.svg'
 
 
 const Navbar = () => {
+
+  const [openClass, setOpenClass] = React.useState(null);
+  const [mobileMenu, setMobileMenu] = React.useState("hidden")
+
+  const buttonClicked = () =>{
+    if (openClass){
+      setOpenClass(null)
+      setMobileMenu("hidden")
+    }else{
+      setOpenClass("open")
+      setMobileMenu("flex")
+
+    }
+  }
+
+
   return (
     // Navbar
     <nav className='relative container mx-auto p-6'>
@@ -23,6 +39,25 @@ const Navbar = () => {
         <a href="" className='hidden md:block p-3 px-6 pt-2 text-white bg-brightRed rounded-full baselie hover:bg-brightRedLight'>
             Get Started
         </a>
+        {/* hamburger icon */}
+        <button id="menu-btn" className={`block ${openClass} hamburger md:hidden focus:outline-none`}
+        onClick={buttonClicked}>
+          <span className='hamburger-top'></span>
+          <span className='hamburger-middle'></span>
+          <span className='hamburger-bottom'></span>
+        </button>
+        </div>
+        {/* MOBILE MENU */}
+        <div className='md:hidden'>
+          <div id='menu' className={`absolute flex-col items-center ${mobileMenu} self-end py-8
+          mt-10 space-y-6 font-bold bg-white sm:w-auto sm:self-center left-6 right-6
+          drop-shadow-md`}>
+            <a href="">Pricing</a>
+            <a href="">Product</a>
+            <a href="">About Us</a>
+            <a href="">Carrers</a>
+            <a href="">Community</a>
+          </div>
         </div>
     </nav>
   )
