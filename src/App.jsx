2c4e0b6fdfc080587eb80e793c6602c5e1cadfa4
vipin <vipinkumarlamba55@gmail.com>
2023-09-15 15:51:59 +0530

    CTA SECTION COMPLETED

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
 import Testimonials from './components/Testimonials'
+import CTASection from './components/CTASection'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <Hero/>
       <Features/>
       <Testimonials/>
+      <CTASection/>
     </>
   )
 }
